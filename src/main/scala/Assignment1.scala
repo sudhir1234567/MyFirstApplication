a1c03689f3191d614381617967cdf559cce3efe0
sudhir1234567 <48981689+sudhir1234567@users.noreply.github.com>
2021-04-14 19:51:50 +0530

    updated the file.
    added double spaces in code.

--- src/main/scala/Assignment1.scala
+++ src/main/scala/Assignment1.scala
@@ -2,6 +2,8 @@ import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
   class Assignment1(val InputDataframe:DataFrame, val Spark:SparkSession) {
 
    val outpath = "C:\\Lookuporc"
+   
+    
 
     //    InputDataframe.show()
     InputDataframe.write.format("orc").option("header","true").option("overwrite","true").save(outpath)
